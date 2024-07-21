9c71afad717a80ce1f7397a7f54e9597ca0e2b4c
Piya <pjg11@users.noreply.github.com>
2024-07-21 20:57:06 +0000

    Problem 2: Means to an End

---
+++ meanstoanend.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/binary"
+	"net"
+
+	"protohackers/server"
+)
+
+type Message struct {
+	Type byte
+	One  int32
+	Two  int32
+}
+
+func meanstoanend(conn net.Conn) {
+	prices := map[int32]int32{}
+
+	for {
+		var message Message
+		err := binary.Read(conn, binary.BigEndian, &message)
+		if err != nil {
+			return
+		}
+
+		switch message.Type {
+
+		case 'I':
+			if _, ok := prices[message.One]; ok {
+				continue
+			}
+			prices[message.One] = message.Two
+
+		case 'Q':
+			sum, count := 0, 0
+			if message.One > message.Two {
+				binary.Write(conn, binary.BigEndian, int32(sum))
+				continue
+			}
+			for timestamp, price := range prices {
+				if timestamp >= message.One && timestamp <= message.Two {
+					sum += int(price)
+					count += 1
+				}
+			}
+			if count > 1 {
+				sum = sum / count
+			}
+			binary.Write(conn, binary.BigEndian, int32(sum))
+
+		default:
+			return
+		}
+	}
+}
+
+func main() {
+	server.Run(meanstoanend)
+}
